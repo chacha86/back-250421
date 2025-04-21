c42efbf09af3254e550a05e19a254715be8e592f
chacha86 <taejincha0619@gmail.com>
2025-04-21 11:29:53 +0900

    work

---
+++ src/main/kotlin/com/think/back250421/domain/home/home/controller/HomeController.kt
@@ -0,0 +1,18 @@
+package com.think.back250421.domain.home.home.controller
+
+import com.think.back250421.domain.home.home.service.HomeService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class HomeController(
+    private val homeService: HomeService
+) {
+
+    @GetMapping("/")
+    fun main(): String {
+        return homeService.hello()
+    }
+
+}
---
+++ src/main/kotlin/com/think/back250421/domain/home/home/service/HomeService.kt
@@ -0,0 +1,16 @@
+package com.think.back250421.domain.home.home.service
+
+import org.springframework.stereotype.Service
+import java.lang.Thread.sleep
+
+@Service
+class HomeService {
+
+    init {
+        sleep(10000)
+    }
+
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
